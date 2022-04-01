d520da30eb20416bff3c2765ca38323fe3299156
Rafael <rafaelvictormuniz@gmail.com>
2022-04-01 20:23:14 -0300

    Ajustes

--- FinanceiroMain/src/Model/bean/FinanceiroMain.java
+++ FinanceiroMain/src/Model/bean/FinanceiroMain.java
@@ -1,5 +1,7 @@
 package Model.bean;
 
+import Model.Dao.PessoaDAO;
+
 public class FinanceiroMain {
 
     public static void main(String[] args) {
@@ -7,26 +9,25 @@ public class FinanceiroMain {
         Pessoa p1 = new Pessoa("Antonio Fulano", "4.477.843", "1990-5-12");
         Pessoa p2 = new Pessoa("Raissa Lara", "3.274.912", "1974-8-23");
         Pessoa p3 = new Pessoa("Arthur Ciclano", "4.556.231", "1989-1-6");
+        PessoaDAO dao = new PessoaDAO();
+        
+        dao.read();
+        
+        //dao.create(p3);
 
+        
+        /*
         Banco registros = new Banco();
         
         Conta c1 = new Conta(p1);
         Conta c2 = new Conta(p2);
         Conta c3 = new Conta(p3);
-        
-        c1.adicionarSaldo(200);
-        c2.adicionarSaldo(500);
-        c2.adicionarSaldo(700);
 
         registros.inserirCliente(c1);
         registros.inserirCliente(c2);
         registros.inserirCliente(c3);
-
-        System.out.println(registros.toString());
+        */
         
-        c2.removerSaldo(1000);
-        
-        System.out.println(registros.toString());
 
     }
 }
