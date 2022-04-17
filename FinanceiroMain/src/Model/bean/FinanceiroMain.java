22e029f108fb8a31f22793d03ac410ef95cb59a8
Rafael <rafaelvictormuniz@gmail.com>
2022-04-17 19:24:45 -0300

    Update FinanceiroMain.java

--- FinanceiroMain/src/Model/bean/FinanceiroMain.java
+++ FinanceiroMain/src/Model/bean/FinanceiroMain.java
@@ -1,5 +1,6 @@
 package Model.bean;
 
+import Model.Dao.ContaDAO;
 import Model.Dao.PessoaDAO;
 
 public class FinanceiroMain {
@@ -9,18 +10,30 @@ public class FinanceiroMain {
         Pessoa p1 = new Pessoa("Antonio Fulano", "4.477.843", "1990-5-12");
         Pessoa p2 = new Pessoa("Raissa Lara", "3.274.912", "1974-8-23");
         Pessoa p3 = new Pessoa("Arthur Ciclano", "4.556.231", "1989-1-6");
-        PessoaDAO dao = new PessoaDAO();
+        PessoaDAO daoPessoa = new PessoaDAO();
+        ContaDAO daoConta = new ContaDAO();
+
         
         //dao.delete(p1);
-        dao.read();
+        //dao.readPessoa();
         
         //dao.create(p3);
 
         
-        /*
+        
         Banco registros = new Banco();
         
-        Conta c1 = new Conta(p1);
+        Conta c2 = new Conta(p2, "CC");
+        
+       
+
+        
+        daoConta.insertConta(c2);
+        
+        
+        
+        
+        /*
         Conta c2 = new Conta(p2);
         Conta c3 = new Conta(p3);
 
